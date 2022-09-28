cc636fd05ce0fb52a7031ca462ca8b94ef0500ef
Tanin Rahman <taninr87@gmail.com>
2022-09-28 22:01:53 +0600

    add fake json and create folder structure

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h2>Listen-The-Best</h2>
+      
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/Songs/Songs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Songs = () => {
+  return (
+    <div>
+      <h3>Select Todays Best</h3>
+    </div>
+  );
+};
+
+export default Songs;
