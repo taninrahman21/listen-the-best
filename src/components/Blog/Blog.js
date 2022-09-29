e1e08b29bf184bd0ad55db6e6a2728f9c49e46df
Tanin Rahman <taninr87@gmail.com>
2022-09-29 12:49:52 +0600

    done all the problems and ready to submit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Blog.css';
 
 const Blog = () => {
   return (
     <div>
+      <h1 className='blogs-title'>Here is some hot questions about React.</h1>
       <div className="blog">
         <h2>How does React Work?</h2>
         <p>React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</p>
